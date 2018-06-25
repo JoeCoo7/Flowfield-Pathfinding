f591f9edd1d39534a32876f5aee44770ef02ab48
Ryan Caltabiano <ryanc@unity3d.com>
2018-06-25 17:33:49 +0200

    Switching to int, and in2 for heatmap

--- Flowfield-Pathfinding/Assets/Scripts/HeatmapToFlowField.cs
+++ Flowfield-Pathfinding/Assets/Scripts/HeatmapToFlowField.cs
@@ -1,37 +1,34 @@
 ﻿using Unity.Collections;
+using Unity.Mathematics;
 
 public static class HeatmapToFlowField
 {
-    static byte min(byte a, byte b)
-    {
-        return a < b ? a : b;
-    }
 
-    public static NativeArray<byte> Compute(GridSettings settings, NativeArray<NativeArray<byte>> heatmaps)
+    public static NativeArray<byte> Compute(GridSettings settings, NativeArray<int>[] heatmaps)
     {
         var root = heatmaps[0];
         for (int i = 1; i < heatmaps.Length; ++i)
         {
             for (int j = 0; j < root.Length; ++j)
-                root[j] = min(root[j], heatmaps[i][j]);
+                root[j] = math.min(root[j], heatmaps[i][j]);
         }
 
         return Compute(settings, root);
     }
 
 
-    public static NativeArray<byte> Compute(GridSettings settings, NativeArray<byte> heatmap)
+    public static NativeArray<byte> Compute(GridSettings settings, NativeArray<int> heatmap)
     {
         NativeArray<byte> flowField = new NativeArray<byte>(heatmap.Length, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
 
         for (int i = 0; i < heatmap.Length; ++i)
         {
-            byte weight = heatmap[i];
+            int weight = heatmap[i];
             flowField[i] = (byte)Direction.MAX;
 
             for (byte j = 0; j < (byte)Direction.MAX; ++j)
             {
-                byte neighbor = GridUtilties.Neighbor(settings, heatmap, GridUtilties.Index2Grid(settings, i), GridUtilties.Offset[j]);
+                int neighbor = GridUtilties.Neighbor(settings, heatmap, GridUtilties.Index2Grid(settings, i), GridUtilties.Offset[j]);
                 if (weight <= neighbor)
                     continue;
 
