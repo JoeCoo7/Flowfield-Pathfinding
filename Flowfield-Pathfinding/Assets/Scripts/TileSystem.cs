c99f6322d9106d4b1ee052ceae6b01aed625dcd1
Tim Mowrer <timm@unity3d.com>
2018-06-25 12:41:37 +0200

    Rename cost and direction to simply "value". Add some initialization and archetype.

--- Flowfield-Pathfinding/Assets/Scripts/TileSystem.cs
+++ Flowfield-Pathfinding/Assets/Scripts/TileSystem.cs
@@ -5,21 +5,27 @@ using Unity.Entities;
 
 public struct TileCost : IComponentData
 {
-    public byte cost;
+    public byte value;
 }
 
 public struct TileDirection : IComponentData
 {
-    public byte direction;
+    public byte value;
 }
 
 public struct TileCollision : IComponentData
 {
-    public byte direction;
+    public byte value;
 }
 
 public class TileSystem : ComponentSystem
 {
+    public static EntityArchetype s_TileType;
+
+    int m_Width = 10;
+
+    int m_Height = 10;
+
     struct Tile
     {
         public ComponentDataArray<TileCost> cost;
@@ -31,5 +37,24 @@ public class TileSystem : ComponentSystem
     protected override void OnUpdate()
     {
 
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Initialize()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        s_TileType = entityManager.CreateArchetype(typeof(TileCost), typeof(TileDirection), typeof(TileCollision));
+    }
+
+    void CreateGrid()
+    {
+        var n = m_Width * m_Height;
+        for (int index = 0; index < n; ++index)
+        {
+            PostUpdateCommands.CreateEntity(s_TileType);
+            PostUpdateCommands.SetComponent(new TileCost() { value = 0 });
+            PostUpdateCommands.SetComponent(new TileDirection() { value = 0 });
+            PostUpdateCommands.SetComponent(new TileCollision() { value = 0 });
+        }
     }
 }
