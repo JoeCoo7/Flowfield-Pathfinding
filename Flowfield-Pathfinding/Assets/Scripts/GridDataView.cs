27a5610ac5a24a96b98e22f7ee703b0754422483
Paul Burslem <paulb@unity3d.com>
2018-06-25 13:41:56 +0200

    grid view

--- Flowfield-Pathfinding/Assets/Scripts/GridDataView.cs
+++ Flowfield-Pathfinding/Assets/Scripts/GridDataView.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Mathematics;
 
 public class GridDataView : MonoBehaviour
 {
@@ -12,6 +12,8 @@ public class GridDataView : MonoBehaviour
 	RenderTexture m_texture;
 	ComputeBuffer m_computeBuffer;
 	public ComputeShader m_computeShader;
+	public float m_noiseScale = 3;
+	public float m_contrast = 2;
 	int m_computeMain;
 	// Use this for initialization
 	void Start ()
@@ -26,7 +28,24 @@ public class GridDataView : MonoBehaviour
 		m_computeShader.SetTexture(m_computeMain, "Result", m_texture);
 		m_computeShader.SetInt("Stride", m_size);
 		m_computeBuffer = new ComputeBuffer(m_size * m_size, 4 * 3);
+		var rs = World.Active.GetOrCreateManager<RenderSystem>();
+		rs.Render = new NativeArray<RenderData>(m_size * m_size, Allocator.Persistent);
+		var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+		var entities = new NativeArray<Entity>(m_size * m_size, Allocator.Persistent);
+		var arch = entityManager.CreateArchetype(typeof(TileCost));
+		entityManager.CreateEntity(arch, entities);
 
+		for (int ii = 0; ii < entities.Length; ii++)
+		{
+			int2 coord = new int2(ii % m_size, ii / m_size);
+			float2 per = new float2(coord.x, coord.y) / m_size;
+			var n = Mathf.PerlinNoise(per.x * m_noiseScale, per.y * m_noiseScale) + .15f;
+			float c = (n * 255);
+			float f = ((259 * (c + 255)) / (255 * (259 - c)));
+			c = (f * (c - 128) + 128);
+			entityManager.SetComponentData(entities[ii], new TileCost() { cost = (byte)math.clamp(c, 0, 255) });
+		}
+		entities.Dispose();
 	}
 
 	// Update is called once per frame
@@ -37,9 +56,10 @@ public class GridDataView : MonoBehaviour
 
 	private void LateUpdate()
 	{
-		//wait for job to complete
+		var rs = World.Active.GetOrCreateManager<RenderSystem>();
+		rs.lastJob.Complete();
 
-		//m_computeBuffer.SetData(rs.RenderData);
+		m_computeBuffer.SetData(rs.Render);
 		m_computeShader.SetBuffer(m_computeMain, "colors", m_computeBuffer);
 		m_computeShader.Dispatch(m_computeMain, m_size / 8, m_size / 8, 1);
 	}
---
+++ Flowfield-Pathfinding/Assets/Scripts/RenderSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+[System.Serializable]
+public struct RenderData : IComponentData
+{
+	public float3 color;
+}
+
+public class RenderSystem : JobComponentSystem
+{
+	struct Data
+	{
+		[ReadOnly]
+		public ComponentDataArray<TileCost> Cost;
+		public EntityArray Entity;
+		public int Length;
+	}
+
+	[Inject] Data m_Data;
+	public int Stride;
+	public JobHandle lastJob;
+	public NativeArray<RenderData> Render;
+	protected override JobHandle OnUpdate(JobHandle inputDeps)
+	{
+		lastJob.Complete();
+		return (lastJob = new Job()
+		{
+			Cost = m_Data.Cost,
+			Render = Render,
+			Stride = Stride
+		}.Schedule(m_Data.Length, Stride, inputDeps));
+	}
+
+	[Unity.Burst.BurstCompile]
+	struct Job : IJobParallelFor
+	{
+		public int Stride;
+		[ReadOnly]
+		public ComponentDataArray<TileCost> Cost;
+		public NativeArray<RenderData> Render;
+
+		public void Execute(int i)
+		{
+			Render[i] = new RenderData() { color = Cost[i].cost / 255f };
+		}
+
+	}
+
+}
