6d9e20edb21bc92985521cc57a5993aba6d0bcdc
JoeCool <matthias@rough-sea.com>
2018-06-25 18:16:02 +0200

    Poisson Disc Distribution

--- Flowfield-Pathfinding/Assets/Scripts/AgentSpawingSystem.cs
+++ Flowfield-Pathfinding/Assets/Scripts/AgentSpawingSystem.cs
@@ -1,91 +1,162 @@
 ﻿using RSGLib;
+using RSGLib.ECS;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+//-----------------------------------------------------------------------------
 public class AgentSpawingSystem : ComponentSystem
 {
 	public static EntityArchetype s_AgentType;
-	public const int MAX_UNITS_PER_CLICK = 1;
+	private const int MAX_UNITS_PER_CLICK = 1000;
+	private const float DIST_SIZEX = 100;
+	private const float DIST_SIZEY = 100;
+	private const int MAX_TRIES = 30; // Maximum number of attempts before marking a sample as inactive.
+	private const float CELL_SIZE = 1f;
 
+	private Rect m_DistributionRect;
+	private int m_DistHeight;
+	private int m_DistWidth;
+	private float m_RadiusSquared; // radius squared
+	private NativeArray<float2> m_Grid;
+	private NativeList<float2> m_activeSamples;
+
+	//-----------------------------------------------------------------------------
 	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
 	static void Initialize()
 	{
 		var entityManager = World.Active.GetOrCreateManager<EntityManager>();
 		s_AgentType = entityManager.CreateArchetype(typeof(Position), typeof(Rotation), typeof(TransformMatrix), typeof(MeshInstanceRenderer));
 	}
-	
+
+	//-----------------------------------------------------------------------------
 	protected override void OnUpdate()
 	{
 		if (!Input.GetMouseButtonDown(StandardInput.LEFT_MOUSE_BUTTON)) return;
 		if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity)) return;
 		
+		m_activeSamples = new NativeList<float2>(Allocator.Temp);
+		m_DistHeight = (int)math.floor(DIST_SIZEX / CELL_SIZE);
+		m_DistWidth = (int)math.floor(DIST_SIZEY / CELL_SIZE);
+		m_Grid = new NativeArray<float2>(m_DistWidth * m_DistHeight, Allocator.Temp);
+		
 		for (int index = 0; index < MAX_UNITS_PER_CLICK; index++)
 		{
-			var pos = Random.insideUnitCircle;
-			CreateAgent(new Vector3(pos.x + hit.point.x, 0, pos.y + hit.point.z));
+			InitSampler(new float2(hit.point.x, hit.point.z), DIST_SIZEX, DIST_SIZEY, CELL_SIZE);
+			float2? pos = Sample();
+			if (pos != null)
+				CreateAgent(new Vector3(pos.Value.x + hit.point.x, 0, pos.Value.y + hit.point.z));
 		}
+		
+		m_Grid.Dispose();
+		m_activeSamples.Dispose();
 	}
 
+	//-----------------------------------------------------------------------------
 	private void CreateAgent(float3 _pos)
 	{
 		PostUpdateCommands.CreateEntity(s_AgentType);
-		PostUpdateCommands.SetComponent(new Position() { Value = _pos});
+		PostUpdateCommands.SetComponent(new Position() {Value = _pos});
 		PostUpdateCommands.SetComponent(new Rotation());
 		PostUpdateCommands.SetComponent(new TransformMatrix());
-		PostUpdateCommands.SetSharedComponent(new MeshInstanceRenderer() { mesh = InitializationData.Instance.AgentMesh, material = InitializationData.Instance.AgentMaterial });
-	}
-/*
-	private void generate_poisson(int width, int height, int minDist, int pointsCount)
-	{
-		//Create the grid
-		cellSize = minDist/sqrt(2);
-
-		grid = Grid2D(Point(
-			(ceil(width/cell_size),         //grid width
-				ceil(height/cell_size))));      //grid height
-
-		//RandomQueue works like a queue, except that it
-		//pops a random element from the queue instead of
-		//the element at the head of the queue
-		processList = RandomQueue();
-		samplePoints = List();
-
-		//generate the first point randomly
-		//and updates 
-
-		firstPoint = Point(rand(width), rand(height));
-
-		//update containers
-		processList.push(firstPoint);
-		samplePoints.push(firstPoint);
-		grid[imageToGrid(firstPoint, cellSize)] = firstPoint;
-
-		//generate other points from points in queue.
-		while (not processList.empty())
+		PostUpdateCommands.SetSharedComponent(new MeshInstanceRenderer()
 		{
-			point = processList.pop();
-			for (i = 0; i < pointsCount; i++)
+			mesh = InitializationData.Instance.AgentMesh,
+			material = InitializationData.Instance.AgentMaterial
+		});
+	}
+
+	//-----------------------------------------------------------------------------
+	public void InitSampler(float2 pos, float width, float height, float radius)
+	{
+		m_DistributionRect = new Rect(0, 0, width, height);
+		m_RadiusSquared = radius * radius;
+	}
+
+	//-----------------------------------------------------------------------------
+	public float2? Sample()
+	{
+		// First sample is choosen randomly
+		AddSample(new float2(Random.value * m_DistributionRect.width, Random.value * m_DistributionRect.height));
+
+		while (m_activeSamples.Length > 0)
+		{
+			// Pick a random active sample
+			int i = (int) Random.value * m_activeSamples.Length;
+			float2 sample = m_activeSamples[i];
+
+			// Try `k` random candidates between [radius, 2 * radius] from that sample.
+			for (int j = 0; j < MAX_TRIES; ++j)
 			{
-				newPoint = generateRandomPointAround(point, minDist);
-				//check that the point is in the image region
-				//and no points exists in the point's neighbourhood
-				if (inRectangle(newPoint) and
-					not inNeighbourhood(grid, newPoint, minDist,
-					cellSize))
-				{
-					//update containers
-					processList.push(newPoint);
-					samplePoints.push(newPoint);
-					grid[imageToGrid(newPoint, cellSize)] =  newPoint;
-				}
+
+				float angle = 2 * Mathf.PI * Random.value;
+				// See: http://stackoverflow.com/questions/9048095/create-random-number-within-an-annulus/9048443#9048443
+				float randomNumber = Mathf.Sqrt(Random.value * 3 * m_RadiusSquared + m_RadiusSquared);
+				float2 candidate = sample + randomNumber * new float2(math.cos(angle), math.sin(angle));
+
+				// Accept candidates if it's inside the rect and farther than 2 * radius to any existing sample.
+				if (m_DistributionRect.Contains(candidate) && IsFarEnough(candidate))
+					return AddSample(candidate);
+			}
+
+			// If we couldn't find a valid candidate after k attempts, remove this sample from the active samples queue
+			m_activeSamples[i] = m_activeSamples[m_activeSamples.Length - 1];
+			m_activeSamples.RemoveAtSwapBack(m_activeSamples.Length - 1);
+		}
+		return null;
+	}
+	
+	//-----------------------------------------------------------------------------
+	// Note: we use the zero vector to denote an unfilled cell in the grid. This means that if we were
+	// to randomly pick (0, 0) as a sample, it would be ignored for the purposes of proximity-testing
+	// and we might end up with another sample too close from (0, 0). This is a very minor issue.
+	private bool IsFarEnough(float2 sample)
+	{
+		GridPos pos = new GridPos(sample, CELL_SIZE);
+		int xmin = Mathf.Max(pos.x - 2, 0);
+		int ymin = Mathf.Max(pos.y - 2, 0);
+		int xmax = Mathf.Min(pos.x + 2, m_DistWidth - 1);
+		int ymax = Mathf.Min(pos.y + 2, m_DistHeight - 1);
+		for (int y = ymin; y <= ymax; y++) 
+		{
+			for (int x = xmin; x <= xmax; x++) 
+			{
+				float2 cell = m_Grid[y * m_DistWidth + x];
+				if (cell.Equals(MathExt.Float2Zero()))
+					continue;
+				
+				float2 d = cell - sample;
+				if (d.x * d.x + d.y * d.y < m_RadiusSquared) 
+					return false;
 			}
 		}
-		return samplePoints;
+		return true;
 	}
-*/
+	//-----------------------------------------------------------------------------
+	
+	/// Adds the sample to the active samples queue and the grid before returning it
+	private Vector2 AddSample(float2 sample)
+	{
+		m_activeSamples.Add(sample);
+		GridPos pos = new GridPos(sample, CELL_SIZE);
+		m_Grid[pos.y * m_DistWidth + pos.x] = sample;
+		return sample;
+	}
 
+	/// Helper struct to calculate the x and y indices of a sample in the grid
+	private struct GridPos
+	{
+		public int x;
+		public int y;
+		public GridPos(float2 sample, float cellSize)
+		{
+			x = (int)(sample.x / cellSize);
+			y = (int)(sample.y / cellSize);
+		}
+	}	
 	
 }
---
+++ Flowfield-Pathfinding/Assets/Scripts/MathExt.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+
+namespace RSGLib.ECS
+{
+    public static class MathExt
+    {
+        public static float2 Float2Zero() { return new float2(0f, 0f);}
+        public static float2 Float2One() { return new float2(1f, 1f); }
+        public static float2 Float2Up() { return new float2(0f, 1f); }
+        public static float2 Float2Down() { return new float2(0f, -1f); }
+        public static float2 Float2Left() { return new float2(-1f, 0f); }
+        public static float2 Float2Right() { return new float2(1f, 0f); }
+        public static float3 Float3Zero() { return new float3(0f, 0f, 0f); }
+        public static float3 Float3One() { return new float3(1f, 1f, 1f); }
+        public static float3 Float3Up() { return new float3(0f, 1f, 0f); }
+        public static float3 Float3Down() { return new float3(0f, -1f, 0f); }
+        public static float3 Float3Left() { return new float3(-1f, 0f, 0f); }
+        public static float3 Float3Right() { return new float3(1f, 0f, 0f); }
+        public static float3 Float3Forward() { return new float3(0f, 0f, 1f); }
+        public static float3 Float3Back() { return new float3(0f, 0f, -1f); }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static float SignedAngle(float2 _from, float2 _to)
+        {
+            float2 from = math.normalize(_from);
+            float2 to = math.normalize(_to);
+            float num = math.acos(math.clamp(math.dot(from, to), -1f, 1f)) * 57.29578f;
+            float num2 = math.sign(from.x * to.y - from.y * to.x);
+            return num * num2;
+        }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static float2 PointOnCircle(float _radius, float _degAngle, float2 _origin)
+        {
+            float radAngle = math.radians(_degAngle);
+            return new float2(_radius * math.cos(radAngle) + _origin.x, _radius * math.sin(radAngle) + _origin.y);
+        }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static bool CirclesCollide(float2 _origin1, float _radius1, float2 _origin2, float _radius2)
+        {
+            float radius = _radius1 + _radius2;
+            float deltaX = _origin1.x - _origin2.x;
+            float deltaY = _origin1.y - _origin2.y; ;
+            return deltaX * deltaX + deltaY * deltaY <= radius * radius;
+        }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static bool CirclesCollide(float3 _origin1, float _radius1, float3 _origin2, float _radius2)
+        {
+            float radius = _radius1 + _radius2;
+            float deltaX = _origin1.x - _origin2.x;
+            float deltaY = _origin1.z - _origin2.z; 
+            return deltaX * deltaX + deltaY * deltaY <= radius * radius;
+        }
+        
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static bool SphereCollision(float3 _origin1, float _radius1, float3 _origin2, float _radius2)
+        {
+            float3 difference = _origin2 - _origin1;
+            float distanceSqr = math.dot(difference, difference);
+            float radiiSqr = _radius1 + _radius2;
+            radiiSqr *= radiiSqr;
+            return distanceSqr <= radiiSqr;
+        }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static float4x4 Invert(float4x4 _value)
+        {
+            var result = new float4x4();
+            float b0 = (_value.c2.x * _value.c3.y) - (_value.c2.y * _value.c3.x);
+            float b1 = (_value.c2.x * _value.c3.z) - (_value.c2.z * _value.c3.x);
+            float b2 = (_value.c2.w * _value.c3.x) - (_value.c2.x * _value.c3.w);
+            float b3 = (_value.c2.y * _value.c3.z) - (_value.c2.z * _value.c3.y);
+            float b4 = (_value.c2.w * _value.c3.y) - (_value.c2.y * _value.c3.w);
+            float b5 = (_value.c2.z * _value.c3.w) - (_value.c2.w * _value.c3.z);
+            float d11 = _value.c1.y * b5 + _value.c1.z * b4 + _value.c1.w * b3;
+            float d12 = _value.c1.x * b5 + _value.c1.z * b2 + _value.c1.w * b1;
+            float d13 = _value.c1.x * -b4 +_value.c1.y * b2 + _value.c1.w * b0;
+            float d14 = _value.c1.x * b3 + _value.c1.y * -b1 +_value.c1.z * b0;
+
+            float det = _value.c0.x * d11 - _value.c0.y * d12 + _value.c0.z * d13 - _value.c0.w * d14;
+            if (Math.Abs(math.abs(det)) < math.epsilon_normal)
+                return float4x4.identity;
+
+            det = 1f / det;
+
+            float a0 = (_value.c0.x * _value.c1.y) - (_value.c0.y * _value.c1.x);
+            float a1 = (_value.c0.x * _value.c1.z) - (_value.c0.z * _value.c1.x);
+            float a2 = (_value.c0.w * _value.c1.x) - (_value.c0.x * _value.c1.w);
+            float a3 = (_value.c0.y * _value.c1.z) - (_value.c0.z * _value.c1.y);
+            float a4 = (_value.c0.w * _value.c1.y) - (_value.c0.y * _value.c1.w);
+            float a5 = (_value.c0.z * _value.c1.w) - (_value.c0.w * _value.c1.z);
+
+            float d21 = _value.c0.y * b5 + _value.c0.z * b4 + _value.c0.w * b3;
+            float d22 = _value.c0.x * b5 + _value.c0.z * b2 + _value.c0.w * b1;
+            float d23 = _value.c0.x * -b4 +_value.c0.y * b2 + _value.c0.w * b0;
+            float d24 = _value.c0.x * b3 + _value.c0.y * -b1 +_value.c0.z * b0;
+            float d31 = _value.c3.y * a5 + _value.c3.z * a4 + _value.c3.w * a3;
+            float d32 = _value.c3.x * a5 + _value.c3.z * a2 + _value.c3.w * a1;
+            float d33 = _value.c3.x * -a4 +_value.c3.y * a2 + _value.c3.w * a0;
+            float d34 = _value.c3.x * a3 + _value.c3.y * -a1 +_value.c3.z * a0;
+            float d41 = _value.c2.y * a5 + _value.c2.z * a4 + _value.c2.w * a3;
+            float d42 = _value.c2.x * a5 + _value.c2.z * a2 + _value.c2.w * a1;
+            float d43 = _value.c2.x * -a4 +_value.c2.y * a2 + _value.c2.w * a0;
+            float d44 = _value.c2.x * a3 + _value.c2.y * -a1 +_value.c2.z * a0;
+
+            result.c0.x = +d11 * det; result.c0.y = -d21 * det; result.c0.z = +d31 * det; result.c0.w = -d41 * det;
+            result.c1.x = -d12 * det; result.c1.y = +d22 * det; result.c1.z = -d32 * det; result.c1.w = +d42 * det;
+            result.c2.x = +d13 * det; result.c2.y = -d23 * det; result.c2.z = +d33 * det; result.c2.w = -d43 * det;
+            result.c3.x = -d14 * det; result.c3.y = +d24 * det; result.c3.z = -d34 * det; result.c3.w = +d44 * det;
+            return result;
+        }   
+        
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static float3 Mul(float4x4 _matrix, float3 _point)
+        {
+            return math.mul(_matrix, new float4(_point, 1)).xyz;
+        }
+        
+        //https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        [MethodImpl((MethodImplOptions) 0x100)] // agressive inline
+        public static quaternion Euler(float _roll, float _pitch, float _yaw)
+        {
+            float cy = math.cos(_yaw * 0.5f);
+            float sy = math.sin(_yaw * 0.5f);
+            float cr = math.cos(_roll * 0.5f);
+            float sr = math.sin(_roll * 0.5f);
+            float cp = math.cos(_pitch * 0.5f);
+            float sp = math.sin(_pitch * 0.5f);
+            return new quaternion(
+                cy * sr * cp - sy * cr * sp,
+                cy * cr * sp + sy * sr * cp,
+                sy * cr * cp - cy * sr * sp,
+                cy * cr * cp + sy * sr * sp);
+        }
+
+        //-----------------------------------------------------------------------------
+        [MethodImpl((MethodImplOptions)0x100)] // agressive inline
+        public static quaternion Euler(float3 _eulers)
+        {
+            return Euler(_eulers.x, _eulers.y, _eulers.z);
+        }
+    }
+}
+
+
