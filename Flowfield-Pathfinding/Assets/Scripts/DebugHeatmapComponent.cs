9b5995f0ece34a80d1555e8d9572c586e6157a69
Paul Burslem <paulb@unity3d.com>
2018-06-26 15:00:57 +0200

    heatmap visualization

--- Flowfield-Pathfinding/Assets/Scripts/DebugHeatmapComponent.cs
+++ Flowfield-Pathfinding/Assets/Scripts/DebugHeatmapComponent.cs
@@ -1,16 +1,17 @@
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 
 namespace DebugHeatmap
 {
     [System.Serializable]
     public struct Component : ISharedComponentData
     {
-        public NativeArray<int> Value;
-    }
+		public NativeArray<int> Value;
+	}
 
-    struct Group
+	struct Group
     {
         public EntityArray entities;
 
