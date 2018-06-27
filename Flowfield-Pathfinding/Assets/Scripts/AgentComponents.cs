f583fe901b81360268edefb915c83bc23e42f2a1
Paul Burslem <paulb@unity3d.com>
2018-06-27 14:12:27 +0200

    fixed max speed div by 0 error

--- Flowfield-Pathfinding/Assets/Scripts/AgentComponents.cs
+++ Flowfield-Pathfinding/Assets/Scripts/AgentComponents.cs
@@ -6,32 +6,3 @@ public struct Velocity : IComponentData
 {
 	public float3 Value;
 }
-
-[System.Serializable]
-public struct AgentSteerParams
-{
-	[Range(0,1000)]
-	public float MaxSpeed;
-	[Range(0, 1000)]
-	public float Acceleration;
-	[Range(0, 10)]
-	public float TerrainFieldWeight;
-	[Range(0, 10)]
-	public float TargetFieldWeight;
-	[Range(.1f, 10)]
-	public float SeparationRadius;
-	[Range(0, 10)]
-	public float SeparationWeight;
-	[Range(0, 10)]
-	public float AlignmentWeight;
-	[Range(0, 10)]
-	public float CohesionWeight;
-	[Range(1, 20)]
-	public float NeighbourHashCellSize;
-	[Range(1, 10)]
-	public float AlignmentRadius;
-	[Range(0, 100)]
-	public float RotationSpeed;
-}
-
-
--- Flowfield-Pathfinding/Assets/Scripts/AgentSteerData.cs
+++ Flowfield-Pathfinding/Assets/Scripts/AgentSteerData.cs
@@ -14,3 +14,34 @@ public class AgentSteerData : ScriptableObject
 #endif
 	public AgentSteerParams m_Data;
 }
+
+
+[System.Serializable]
+public struct AgentSteerParams
+{
+	[Range(.1f, 100)]
+	public float MaxSpeed;
+	[Range(0, 100)]
+	public float Acceleration;
+	[Range(0, 10)]
+	public float TerrainFieldWeight;
+	[Range(0, 10)]
+	public float TargetFieldWeight;
+	[Range(.1f, 10)]
+	public float SeparationRadius;
+	[Range(0, 10)]
+	public float SeparationWeight;
+	[Range(0, 10)]
+	public float AlignmentWeight;
+	[Range(0, 10)]
+	public float CohesionWeight;
+	[Range(1, 20)]
+	public float NeighbourHashCellSize;
+	[Range(1, 10)]
+	public float AlignmentRadius;
+	[Range(0, 100)]
+	public float RotationSpeed;
+}
+
+
+
