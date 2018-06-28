6c65a723c3f1b5c437f4ad14998b15ada003c0d0
Tim Mowrer <timm@unity3d.com>
2018-06-28 10:50:49 +0200

    Display number of agents on the screen

---
+++ Flowfield-Pathfinding/Assets/Scripts/DisplayInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class DisplayInfo : MonoBehaviour
+{
+    void OnGUI()
+    {
+        var agentSystem = World.Active.GetExistingManager<AgentSystem>();
+        if (agentSystem != null)
+        {
+            GUILayout.Label(string.Format("Agents: {0}", agentSystem.numAgents));
+        }
+    }
+}
